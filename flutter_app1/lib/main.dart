0ea3612e0801bb2a2c88c3783cb85ca3ca1a0762
Retr0 <amarok1217@gmail.com>
2018-12-26 18:17:07 +0800

    init

---
+++ flutter_app1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
