60f6c126231a9d862329fbba6565bf5052c64da6
Retr0 <amarok1217@gmail.com>
2018-12-26 19:12:56 +0800

    12261912

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('welcome to Flutter'),
-        ),
+        // appBar: AppBar(
+        //   title: Text('welcome to Flutter'),
+        // ),
         body: Center(
-          child: Text('Hello World'),
+          child: Container(
+            child: new Text(
+              'hello world',
+              style: TextStyle(
+                fontSize: 40.0
+              ),
+            ),
+            alignment: Alignment.bottomCenter,
+            width: 500.0,
+            height: 400.0,
+            color: Colors.lightBlue,
+          ),
+          // child: Text(
+          //   'Hello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbb',
+          //   textAlign: TextAlign.center,
+          //   maxLines: 2,
+          //   overflow: TextOverflow.ellipsis,
+          //   style: TextStyle(
+          //     fontSize: 25.0,
+          //     color: Color.fromARGB(255, 255, 125, 125),
+          //     decoration: TextDecoration.underline,
+          //     decorationStyle: TextDecorationStyle.solid
+          //   ),
+          // ),
         ),
       ),
     );
