431d0dfede2114aacad7dd59b01d6d7f89d06442
Retr0 <amarok1217@163.com>
2018-12-28 14:39:54 +0800

    12271439

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -15,23 +15,35 @@ class MyApp extends StatelessWidget {
         // ),
         body: Center(
           child: Container(
-            child: new Text(
-              'hello world',
-              style: TextStyle(fontSize: 40.0),
+            child: Image.network(
+              'https://i0.hdslb.com/bfs/face/8768c4e2b3b357a3271389150afa2a3e6c19a126.jpg@72w_72h.webp',
+              // fit: BoxFit.cover,
+              // color: Colors.greenAccent,
+              // colorBlendMode: BlendMode.darken,
+              repeat: ImageRepeat.repeatX,
             ),
-            alignment: Alignment.topLeft,
-            width: 500.0,
-            height: 400.0,
-            // color: Colors.lightBlue,
-            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
-            margin: const EdgeInsets.all(30.0),
-            decoration: new BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
-              ),
-              // border: Border.all(width: 2.0,color: Colors.red)
-            ), 
+            width: 300,
+            height: 400,
+            color: Colors.black,
+            // child: new Text(
+            //   'hello world',
+            //   style: TextStyle(fontSize: 40.0),
+            // ),
+            // alignment: Alignment.topLeft,
+            // width: 500.0,
+            // height: 400.0,
+            // // color: Colors.lightBlue,
+            // padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
+            // margin: const EdgeInsets.all(30.0),
+            // decoration: new BoxDecoration(
+            //   gradient: const LinearGradient(
+            //     colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+            //   ),
+            //   // border: Border.all(width: 2.0,color: Colors.red)
+            // ), 
           ),
+
+
           // child: Text(
           //   'Hello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbbHello World , aaaaaaaaaaaaa, bbbbbbbbbbb',
           //   textAlign: TextAlign.center,
